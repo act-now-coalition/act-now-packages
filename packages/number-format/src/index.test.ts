45759ec168f93fa75740b0ee4e7100e7023004eb
chasulin <44076375+chasulin@users.noreply.github.com>
2022-05-26 15:32:56 -0400

    refactors utils

--- packages/number-format/src/index.test.ts
+++ packages/number-format/src/index.test.ts
@@ -8,7 +8,7 @@ import {
 
 describe("numberFormatUtils", () => {
   describe("formatInteger", () => {
-    test("Round to the closest integer", () => {
+    test("Rounds to the closest integer", () => {
       expect(formatInteger(1.1)).toBe("1");
       expect(formatInteger(1.9)).toBe("2");
       expect(formatInteger(15_599)).toBe("15,599");
@@ -17,37 +17,65 @@ describe("numberFormatUtils", () => {
   });
 
   describe("formatDecimal", () => {
-    test("Value to decimal string", () => {
+    test("Value to decimal string, rounds to maximum of 2 decimal places by default", () => {
       expect(formatDecimal(1.2345)).toBe("1.23");
       expect(formatDecimal(1.9)).toBe("1.9");
       expect(formatDecimal(0)).toBe("0");
     });
-    test("Value to decimal string with decimal places specified", () => {
+    test("Respects places param when provided", () => {
       expect(formatDecimal(1.2345, 3)).toBe("1.235");
       expect(formatDecimal(1.2345, 1)).toBe("1.2");
       expect(formatDecimal(0, 3)).toBe("0");
     });
+    test("Respects options param when provided", () => {
+      expect(formatDecimal(1.2345, { minimumFractionDigits: 3 })).toBe("1.235");
+      expect(formatDecimal(1.93, { maximumFractionDigits: 1 })).toBe("1.9");
+      expect(formatDecimal(1.2345, { maximumSignificantDigits: 3 })).toBe(
+        "1.23"
+      );
+      expect(formatDecimal(1.23, { minimumSignificantDigits: 4 })).toBe(
+        "1.230"
+      );
+    });
   });
 
   describe("formatPercent", () => {
-    test("Value to percentage string", () => {
+    test("Value to percentage string, rounds to whole integer by default", () => {
       expect(formatPercent(0.1234)).toBe("12%");
       expect(formatPercent(1.2345)).toBe("123%");
       expect(formatPercent(-0.4321)).toBe("-43%");
       expect(formatPercent(0)).toBe("0%");
     });
-    test("Value to percentage string with decimal places specified", () => {
+    test("Respects places param when provided", () => {
       expect(formatPercent(0.1234, 1)).toBe("12.3%");
       expect(formatPercent(0.1234, 2)).toBe("12.34%");
       expect(formatPercent(0.1234, 3)).toBe("12.34%");
     });
+    test("Respects options param when provided", () => {
+      expect(formatPercent(0.1234, { minimumFractionDigits: 3 })).toBe(
+        "12.340%"
+      );
+      expect(formatPercent(0.1234, { maximumFractionDigits: 1 })).toBe("12.3%");
+      expect(formatPercent(0.1234, { maximumSignificantDigits: 3 })).toBe(
+        "12.3%"
+      );
+      expect(formatPercent(0.1234, { minimumSignificantDigits: 4 })).toBe(
+        "12.34%"
+      );
+    });
   });
 
   describe("formatMillions", () => {
-    test("rounds to 2 decimals by default", () => {
+    test("Rounds to 2 decimal places by default", () => {
       expect(formatMillions(1_555_000)).toBe("1.56");
     });
-    test("respects the formatting options", () => {
+    test("Respects places param when provided", () => {
+      expect(formatMillions(1_555_000, 3)).toBe("1.555");
+      expect(formatMillions(1_555_000, 1)).toBe("1.6");
+      expect(formatMillions(1_555_000, 2)).toBe("1.56");
+      expect(formatMillions(1_555_000, 0)).toBe("2");
+    });
+    test("Respects options param when provided", () => {
       expect(formatMillions(1_555_000, { minimumFractionDigits: 3 })).toBe(
         "1.555"
       );
@@ -64,10 +92,16 @@ describe("numberFormatUtils", () => {
   });
 
   describe("formatBillions", () => {
-    test("rounds to 2 decimals by default", () => {
+    test("Rounds to 2 decimal places by default", () => {
       expect(formatBillions(1_888_000_000)).toBe("1.89");
     });
-    test("respects the formatting options", () => {
+    test("Respects places param when provided", () => {
+      expect(formatBillions(1_555_000_000, 3)).toBe("1.555");
+      expect(formatBillions(1_555_000_000, 1)).toBe("1.6");
+      expect(formatBillions(1_555_000_000, 2)).toBe("1.56");
+      expect(formatBillions(1_555_000_000, 0)).toBe("2");
+    });
+    test("Respects options param when provided", () => {
       expect(formatBillions(1_888_000_000, { minimumFractionDigits: 3 })).toBe(
         "1.888"
       );
--- packages/number-format/src/index.ts
+++ packages/number-format/src/index.ts
@@ -10,32 +10,40 @@
  * @returns Formatted number (as string)
  */
 export function formatInteger(value: number): string {
-  return new Intl.NumberFormat(undefined, {
-    maximumFractionDigits: 0,
-  }).format(value);
+  return value.toLocaleString(undefined, { maximumFractionDigits: 0 });
 }
 
 /**
- * Format a number using fixed point notation, rounding to the amount of decimal places specified
- * by the places param, or rounded to 2 decimal places if no places param is provided.
+ * Format a number using fixed point notation.
  *
  * @example
  * formatDecimal(1.2345)    // "1.23"
  * formatDecimal(1.2345, 3) // "1.235"
  *
  * @param value Number to format
- * @param places Number of decimal places to round to (defaults to 2)
+ * @param places (optional) Number of decimal places to round to (defaults to 2)
+ * @param options (optional) Object of international number formatting options (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat)
  * @returns Formatted decimal (as string)
  */
-export function formatDecimal(value: number, places = 2): string {
-  return new Intl.NumberFormat(undefined, {
-    maximumFractionDigits: places,
-  }).format(value);
+
+export function formatDecimal(value: number, places?: number): string;
+export function formatDecimal(
+  value: number,
+  options?: Intl.NumberFormatOptions
+): string;
+export function formatDecimal(
+  value: number,
+  placesOrOptions: number | Intl.NumberFormatOptions = 2
+): string {
+  return typeof placesOrOptions === "number"
+    ? new Intl.NumberFormat(undefined, {
+        maximumFractionDigits: placesOrOptions,
+      }).format(value)
+    : new Intl.NumberFormat(undefined, placesOrOptions).format(value);
 }
 
 /**
- * Format a percentage representation of a number, rounding to the amount of decimal places specified
- * by the places param, or rounded to the nearest whole integer if no places param is provided.
+ * Format a percentage representation of a number.
  *
  * The number is multiplied by 100 to create a percentage.
  *
@@ -45,57 +53,87 @@ export function formatDecimal(value: number, places = 2): string {
  * formatPercent(0.8567, 2) // 85.67%
  *
  * @param value Number to format
- * @param places Number of decimal places to round to (defaults to 0)
+ * @param places (optional) Number of decimal places to round to (defaults to 0)
+ * @param options (optional) Object of international number formatting options (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat)
  * @returns Formatted percentage (as string)
  */
 
-export function formatPercent(value: number, places = 0): string {
-  return new Intl.NumberFormat(undefined, {
-    style: "percent",
-    maximumFractionDigits: places,
-  }).format(value);
+export function formatPercent(value: number, places?: number): string;
+export function formatPercent(
+  value: number,
+  options?: Intl.NumberFormatOptions
+): string;
+export function formatPercent(
+  value: number,
+  placesOrOptions: number | Intl.NumberFormatOptions = 0
+): string {
+  return typeof placesOrOptions === "number"
+    ? new Intl.NumberFormat(undefined, {
+        style: "percent",
+        maximumFractionDigits: placesOrOptions,
+      }).format(value)
+    : new Intl.NumberFormat(undefined, {
+        style: "percent",
+        ...placesOrOptions,
+      }).format(value);
 }
 
 /**
- * Format a number in millions. It receives (optionally) the same options
- * as `Intl.NumberFormat`.
+ * Format a number in millions.
  *
  * @example
  * formatMillions(1_555_000) // "1.56"
  * formatMillions(1_555_000, { minimumFractionDigits: 3 }) // "1.555"
  *
  * @param value Number to format
- * @param options Formatting options
+ * @param places (optional) Number of decimal places to round to (defaults to 3)
+ * @param options (optional) Object of international number formatting options (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat)
  * @returns Formatted number (as string)
  */
+
+export function formatMillions(value: number, places?: number): string;
 export function formatMillions(
   value: number,
-  options?: Intl.NumberFormatOptions | undefined
+  options?: Intl.NumberFormatOptions
+): string;
+export function formatMillions(
+  value: number,
+  placesOrOptions: number | Intl.NumberFormatOptions = 2
 ): string {
-  return new Intl.NumberFormat(
-    undefined,
-    options ?? { maximumSignificantDigits: 3 }
-  ).format(value / 1_000_000);
+  const millifiedValue = value / 1_000_000;
+  return typeof placesOrOptions === "number"
+    ? new Intl.NumberFormat(undefined, {
+        maximumFractionDigits: placesOrOptions,
+      }).format(millifiedValue)
+    : new Intl.NumberFormat(undefined, placesOrOptions).format(millifiedValue);
 }
 
 /**
- * Format a number in billions. It receives (optionally) the same options
- * as `Intl.NumberFormat`.
+ * Format a number in billions.
  *
  * @example
  * formatBillions(1_888_000_000) // "1.88"
  * formatBillions(1_888_000_000, { minimumFractionDigits: 3 }) // "1.888"
  *
  * @param value Number to format
- * @param options Formatting options
+ * @param places (optional) Number of decimal places to round to (defaults to 3)
+ * @param options (optional) Object of international number formatting options (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat)
  * @returns Formatted number (as string)
  */
+
+export function formatBillions(value: number, places?: number): string;
 export function formatBillions(
   value: number,
-  options?: Intl.NumberFormatOptions | undefined
+  options?: Intl.NumberFormatOptions
+): string;
+export function formatBillions(
+  value: number,
+  placesOrOptions: number | Intl.NumberFormatOptions = 2
 ): string {
-  return new Intl.NumberFormat(
-    undefined,
-    options ?? { maximumSignificantDigits: 3 }
-  ).format(value / 1_000_000_000);
+  const billifiedValue = value / 1_000_000_000;
+  return typeof placesOrOptions === "number"
+    ? new Intl.NumberFormat(undefined, {
+        maximumFractionDigits: placesOrOptions,
+      }).format(billifiedValue)
+    : new Intl.NumberFormat(undefined, placesOrOptions).format(billifiedValue);
 }
