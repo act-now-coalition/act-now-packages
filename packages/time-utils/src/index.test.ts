c12e5244ecda691c43606e5b45cee749e7819b91
Fai <fai@covidactnow.org>
2022-05-31 14:56:31 -0700

    Address comments

--- packages/time-utils/src/index.test.ts
+++ packages/time-utils/src/index.test.ts
@@ -11,78 +11,105 @@ import {
   getTimeDiff,
 } from "./index";
 
-// Date object constructor takes a zero-indexed month number.
+/**
+ * Helper to create Date instances from a numeric year, month, and day.
+ *
+ * This mostly exists because the Date constructor expects 0-indexed months,
+ * which is pretty unnatural.
+ *
+ * The resulting Date will be set to midnight local (Pacific) time.
+ *
+ * @param year Numeric year (e.g. 2020)
+ * @param month The 1-indexed month (e.g. 3 for March)
+ * @param day 1-indexed day (e.g. 25 for the 25th of March).
+ * @returns The created Date object.
+ */
+function createDate(
+  year: number,
+  month: number,
+  day: number,
+  hour?: number,
+  minute?: number
+) {
+  if (hour && minute) {
+    return new Date(year, month - 1, day, hour, minute);
+  } else if (hour) {
+    return new Date(year, month - 1, day, hour);
+  } else return new Date(year, month - 1, day);
+}
 
-describe("time formatting", () => {
-  const testDate = new Date(2020, 2, 1);
+describe("date/time formatting", () => {
+  const testDate = createDate(2020, 3, 1);
+
   test("Date in ISO format", () => {
     expect(formatDateTime(testDate, DateFormat.YYYY_MM_DD)).toBe("2020-03-01");
   });
-  test("Dash separated date", () => {
+
+  test("Slash separated date", () => {
     expect(formatDateTime(testDate, DateFormat.MM_DD_YYYY)).toBe("03/01/2020");
   });
+
   test("Date in full month, day, year", () => {
     expect(formatDateTime(testDate, DateFormat.MMMM_D_YYYY)).toBe(
       "March 1, 2020"
     );
   });
+
   test("Date in shorthand month, day, year", () => {
     expect(formatDateTime(testDate, DateFormat.MMM_DD_YYYY)).toBe(
       "Mar 01, 2020"
     );
   });
+
   test("Date in full month and unpadded day", () => {
     expect(formatDateTime(testDate, DateFormat.MMMM_D)).toBe("March 1");
-  });
-  test("Past date", () => {
-    expect(formatDateTime(new Date(2015, 1, 1), DateFormat.YYYY_MM_DD)).toBe(
-      "2015-02-01"
-    );
-  });
-  test("Future date", () => {
-    expect(formatDateTime(new Date(2025, 1, 1), DateFormat.YYYY_MM_DD)).toBe(
-      "2025-02-01"
-    );
   });
 });
 
 describe("utc time formatting", () => {
   // Pacific timezone is set as the default in package.json for testing consistency.
-  const testDate = new Date(2020, 2, 1, 20); // March 1st 2020 8 pm. Pacific.
+  const testDate = createDate(2020, 3, 1);
+
   test("Date in ISO format", () => {
     expect(formatUTCDateTime(testDate, DateFormat.YYYY_MM_DD)).toBe(
-      "2020-03-02"
+      "2020-03-01"
     );
   });
-  test("Dash separated date", () => {
+
+  test("Slash separated date", () => {
     expect(formatUTCDateTime(testDate, DateFormat.MM_DD_YYYY)).toBe(
-      "03/02/2020"
+      "03/01/2020"
     );
   });
+
   test("Date in full month, day, year", () => {
     expect(formatUTCDateTime(testDate, DateFormat.MMMM_D_YYYY)).toBe(
-      "March 2, 2020"
+      "March 1, 2020"
     );
   });
+
   test("Date in shorthand month, day, year", () => {
     expect(formatUTCDateTime(testDate, DateFormat.MMM_DD_YYYY)).toBe(
-      "Mar 02, 2020"
+      "Mar 01, 2020"
     );
   });
+
   test("Date in full month and unpadded day", () => {
-    expect(formatUTCDateTime(testDate, DateFormat.MMMM_D)).toBe("March 2");
+    expect(formatUTCDateTime(testDate, DateFormat.MMMM_D)).toBe("March 1");
   });
 });
 
 describe("date string parsing", () => {
   test("ISO format to date object", () => {
     expect(parseDateString("2020-03-01T00:00:00.000")).toEqual(
-      new Date(2020, 2, 1)
+      createDate(2020, 3, 1)
     );
   });
+
   test("Date string to date object", () => {
-    expect(parseDateString("2020-03-01")).toEqual(new Date(2020, 2, 1));
+    expect(parseDateString("2020-03-01")).toEqual(createDate(2020, 3, 1));
   });
+
   test("Invalid date string", () => {
     expect(() => parseDateString("Hello")).toThrow();
   });
@@ -95,6 +122,7 @@ describe("unix time parsing", () => {
       "2020-03-01T08:00:00.000Z"
     );
   });
+
   test("unix time with non-zero minutes and seconds", () => {
     expect(parseDateUnix(1583080230000).toISOString()).toBe(
       "2020-03-01T16:30:30.000Z"
@@ -104,23 +132,26 @@ describe("unix time parsing", () => {
 
 describe("add time", () => {
   test("Add hours", () => {
-    expect(addTime(new Date(2020, 2, 1), 8, TimeUnit.HOURS).toISOString()).toBe(
-      "2020-03-01T16:00:00.000Z"
-    );
+    expect(
+      addTime(createDate(2020, 3, 1), 8, TimeUnit.HOURS).toISOString()
+    ).toBe("2020-03-01T16:00:00.000Z");
   });
+
   test("Add days", () => {
-    expect(addTime(new Date(2020, 2, 1), 5, TimeUnit.DAYS)).toEqual(
-      new Date(2020, 2, 6)
+    expect(addTime(createDate(2020, 3, 1), 5, TimeUnit.DAYS)).toEqual(
+      createDate(2020, 3, 6)
     );
   });
+
   test("Add weeks", () => {
-    expect(addTime(new Date(2020, 2, 1), 2, TimeUnit.WEEKS)).toEqual(
-      new Date(2020, 2, 15)
+    expect(addTime(createDate(2020, 3, 1), 2, TimeUnit.WEEKS)).toEqual(
+      createDate(2020, 3, 15)
     );
   });
+
   test("Add months", () => {
-    expect(addTime(new Date(2020, 2, 1), 3, TimeUnit.MONTHS)).toEqual(
-      new Date(2020, 5, 1)
+    expect(addTime(createDate(2020, 3, 1), 3, TimeUnit.MONTHS)).toEqual(
+      createDate(2020, 6, 1)
     );
   });
 });
@@ -128,48 +159,54 @@ describe("add time", () => {
 describe("subtract time", () => {
   test("Subtract hours", () => {
     expect(
-      subtractTime(new Date(2020, 2, 1), 8, TimeUnit.HOURS).toISOString()
+      subtractTime(createDate(2020, 3, 1), 8, TimeUnit.HOURS).toISOString()
     ).toBe("2020-03-01T00:00:00.000Z");
   });
+
   test("Subtract days", () => {
-    expect(subtractTime(new Date(2020, 2, 1), 5, TimeUnit.DAYS)).toEqual(
-      new Date(2020, 1, 25)
+    expect(subtractTime(createDate(2020, 3, 1), 5, TimeUnit.DAYS)).toEqual(
+      createDate(2020, 2, 25)
     );
   });
+
   test("Subtract weeks", () => {
-    expect(subtractTime(new Date(2020, 2, 1), 2, TimeUnit.WEEKS)).toEqual(
-      new Date(2020, 1, 16)
+    expect(subtractTime(createDate(2020, 3, 1), 2, TimeUnit.WEEKS)).toEqual(
+      createDate(2020, 2, 16)
     );
   });
+
   test("Subtract months", () => {
-    expect(subtractTime(new Date(2020, 2, 1), 3, TimeUnit.MONTHS)).toEqual(
-      new Date(2019, 11, 1)
+    expect(subtractTime(createDate(2020, 3, 1), 3, TimeUnit.MONTHS)).toEqual(
+      createDate(2019, 12, 1)
     );
   });
 });
 
 describe("get start of time", () => {
   test("Start of hour", () => {
-    expect(getStartOf(new Date(2020, 2, 1, 0, 30), TimeUnit.HOURS)).toEqual(
-      new Date(2020, 2, 1, 0, 0)
+    expect(getStartOf(createDate(2020, 3, 1, 0, 30), TimeUnit.HOURS)).toEqual(
+      createDate(2020, 3, 1, 0, 0)
     );
   });
+
   test("Start of day", () => {
-    expect(getStartOf(new Date(2020, 2, 1, 8, 30), TimeUnit.DAYS)).toEqual(
-      new Date(2020, 2, 1, 0, 0)
+    expect(getStartOf(createDate(2020, 3, 1, 8, 30), TimeUnit.DAYS)).toEqual(
+      createDate(2020, 3, 1, 0, 0)
     );
   });
+
   // Start of week test will need to be adjusted when implementing Luxon migration as
   // moment treats Sunday as start of week and luxon treats Monday as start of week.
   // Currently, no start of week function calls are implemented.
   test("Start of week", () => {
-    expect(getStartOf(new Date(2020, 2, 5), TimeUnit.WEEKS)).toEqual(
-      new Date(2020, 2, 2)
+    expect(getStartOf(createDate(2020, 3, 5), TimeUnit.WEEKS)).toEqual(
+      createDate(2020, 3, 2)
     );
   });
+
   test("Start of month", () => {
-    expect(getStartOf(new Date(2020, 2, 15), TimeUnit.MONTHS)).toEqual(
-      new Date(2020, 2, 1)
+    expect(getStartOf(createDate(2020, 3, 15), TimeUnit.MONTHS)).toEqual(
+      createDate(2020, 3, 1)
     );
   });
 });
@@ -177,46 +214,77 @@ describe("get start of time", () => {
 describe("get time difference", () => {
   test("Negative difference in hours", () => {
     expect(
-      getTimeDiff(new Date(2020, 2, 1), new Date(2020, 2, 1, 8), TimeUnit.HOURS)
+      getTimeDiff(
+        createDate(2020, 3, 1),
+        createDate(2020, 3, 1, 8),
+        TimeUnit.HOURS
+      )
     ).toEqual(-8);
   });
+
   test("Positive difference in hours", () => {
     expect(
       getTimeDiff(
-        new Date(2020, 2, 1, 15),
-        new Date(2020, 2, 1, 10),
+        createDate(2020, 3, 1, 15),
+        createDate(2020, 3, 1, 10),
         TimeUnit.HOURS
       )
     ).toEqual(5);
   });
+
   test("Negative difference in days", () => {
     expect(
-      getTimeDiff(new Date(2020, 2, 1), new Date(2020, 2, 5), TimeUnit.DAYS)
+      getTimeDiff(createDate(2020, 3, 1), createDate(2020, 3, 5), TimeUnit.DAYS)
     ).toEqual(-4);
   });
+
   test("Positive difference in days", () => {
     expect(
-      getTimeDiff(new Date(2020, 2, 10), new Date(2020, 2, 7), TimeUnit.DAYS)
+      getTimeDiff(
+        createDate(2020, 3, 10),
+        createDate(2020, 3, 7),
+        TimeUnit.DAYS
+      )
     ).toEqual(3);
   });
+
   test("Negative difference in weeks", () => {
     expect(
-      getTimeDiff(new Date(2020, 2, 1), new Date(2020, 2, 15), TimeUnit.WEEKS)
+      getTimeDiff(
+        createDate(2020, 3, 1),
+        createDate(2020, 3, 15),
+        TimeUnit.WEEKS
+      )
     ).toEqual(-2);
   });
+
   test("Positive difference in weeks", () => {
     expect(
-      getTimeDiff(new Date(2020, 2, 29), new Date(2020, 2, 15), TimeUnit.WEEKS)
+      getTimeDiff(
+        createDate(2020, 3, 29),
+        createDate(2020, 3, 15),
+        TimeUnit.WEEKS
+      )
     ).toEqual(2);
   });
+
   test("Negative difference in months", () => {
     expect(
-      getTimeDiff(new Date(2020, 2, 1), new Date(2020, 5, 1), TimeUnit.MONTHS)
+      getTimeDiff(
+        createDate(2020, 3, 1),
+        createDate(2020, 6, 1),
+        TimeUnit.MONTHS
+      )
     ).toEqual(-3);
   });
+
   test("Positive difference in months", () => {
     expect(
-      getTimeDiff(new Date(2020, 10, 1), new Date(2020, 5, 1), TimeUnit.MONTHS)
+      getTimeDiff(
+        createDate(2020, 11, 1),
+        createDate(2020, 6, 1),
+        TimeUnit.MONTHS
+      )
     ).toEqual(5);
   });
 });
