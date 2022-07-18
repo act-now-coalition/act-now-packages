2a640fcb00ed1f055c224fb0efcc84e43b8a3872
sean mcclure <55333380+smcclure17@users.noreply.github.com>
2022-07-18 18:28:59 -0400

    Add toJSON and fromJSON mathods to Region class (#12)
    * Add toJSON and fromJSON mathods to Region
    
    * Fix parents, add unit tests
    
    * Update version number, update test region data

--- packages/regions/src/Region.test.ts
+++ packages/regions/src/Region.test.ts
@@ -1,8 +1,46 @@
-import { Region } from "./Region";
+import { Region, RegionJSON } from "./Region";
 
 describe("Region", () => {
   test("toSlug", () => {
     expect(Region.toSlug("Río Grande")).toBe("rio_grande");
     expect(Region.toSlug("DeSoto County")).toBe("de_soto_county");
+  });
+});
+
+describe("Region serialization", () => {
+  const parentJSON: RegionJSON = {
+    regionId: "12",
+    fullName: "Florida",
+    shortName: "Florida",
+    abbreviation: "FL",
+    urlFragment: "us/florida-fl",
+    parent: null,
+    population: 21_477_737,
+  };
+
+  const regionJSON: RegionJSON = {
+    regionId: "12089",
+    fullName: "Miami-Dade County, Florida",
+    shortName: "Miami-Dade County, FL",
+    abbreviation: "Miami-Dade Co.",
+    urlFragment: "miami_dade_co",
+    parent: parentJSON,
+    population: 2_716_940,
+  };
+  const region = new Region(
+    "12089",
+    "Miami-Dade County, Florida",
+    "Miami-Dade County, FL",
+    "Miami-Dade Co.",
+    "miami_dade_co",
+    Region.fromJSON(parentJSON),
+    2_716_940
+  );
+
+  test("toJSON", () => {
+    expect(region.toJSON()).toStrictEqual(regionJSON);
+  });
+  test("fromJSON", () => {
+    expect(Region.fromJSON(regionJSON)).toStrictEqual(region);
   });
 });
