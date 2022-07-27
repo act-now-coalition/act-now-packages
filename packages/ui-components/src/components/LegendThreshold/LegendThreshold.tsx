e6c8de53633c040b2985feda6d3941088c878ab9
Pablo Navarro <pnavarrc@gmail.com>
2022-07-27 13:29:14 -0700

    ui-components: LegendThreshold is now an SVG element (#19)

--- packages/ui-components/src/components/LegendThreshold/LegendThreshold.tsx
+++ packages/ui-components/src/components/LegendThreshold/LegendThreshold.tsx
@@ -33,12 +33,8 @@ export interface LegendThresholdProps<T> {
 }
 
 /**
- * `LegendThreshold` represents a scale with specific thresholds that separate
- * a set of levels. By default, the component shows labels between each level.
- *
- * Notes: This component returns an SVG Group element to make it easier to
- * integrate with other components inside an SVG element. Make sure to wrap
- * it in an SVG element if using it as standalone component.
+ * `LegendThreshold` represents a scale with thresholds that separate
+ * a set of levels. By default, the labels between each level are shown.
  */
 const LegendThreshold = <T,>({
   height = 40,
@@ -49,7 +45,9 @@ const LegendThreshold = <T,>({
   getItemColor,
   getItemEndLabel,
   showLabels = true,
-}: LegendThresholdProps<T>) => {
+  ...otherSvgProps
+}: LegendThresholdProps<T> &
+  Omit<React.SVGProps<SVGSVGElement>, keyof LegendThresholdProps<T>>) => {
   const indexList = items.map((item, itemIndex) => itemIndex);
   const scaleRect = scaleBand({ domain: indexList, range: [0, width] });
   const rectWidth = scaleRect.bandwidth();
@@ -60,7 +58,7 @@ const LegendThreshold = <T,>({
   const tickLabelPadding = 2;
 
   return (
-    <Group>
+    <svg width={width} height={height} {...otherSvgProps}>
       <defs>
         <clipPath id={clipPathId}>
           <rect
@@ -96,7 +94,7 @@ const LegendThreshold = <T,>({
           );
         })}
       </Group>
-    </Group>
+    </svg>
   );
 };
 
