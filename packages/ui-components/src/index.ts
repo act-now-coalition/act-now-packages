087bfee9ce3a6ef6d6ccf0278cf963425aee1559
chasulin <44076375+chasulin@users.noreply.github.com>
2022-08-01 16:52:12 -0400

    fix plopfile

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -1,3 +1,3 @@
 export { default as LegendThreshold } from "./components/LegendThreshold";
 
-export { default as LegendCategorical } from "components/LegendCategorical";
+export { default as LegendCategorical } from "./components/LegendCategorical";
