0d40c7c3a5a5aa4cf58f24db55f0e9f79378c250
Michael Lehenbauer <mikelehen@gmail.com>
2022-08-02 17:25:38 -0700

    Fix LegendCategorical docs.

--- packages/ui-components/src/components/LegendCategorical/index.ts
+++ packages/ui-components/src/components/LegendCategorical/index.ts
@@ -1,1 +1,3 @@
 export { default } from "./LegendCategorical";
+
+export type { LegendCategoricalProps } from "./LegendCategorical";
--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -2,3 +2,4 @@ export { default as LegendThreshold } from "./components/LegendThreshold";
 export type { LegendThresholdProps } from "./components/LegendThreshold";
 
 export { default as LegendCategorical } from "./components/LegendCategorical";
+export type { LegendCategoricalProps } from "./components/LegendCategorical";
