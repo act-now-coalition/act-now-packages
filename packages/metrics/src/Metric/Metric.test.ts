b436d3e5977da71a3b798264817e9a8056ecbc58
Michael Lehenbauer <mikelehen@gmail.com>
2022-08-06 07:31:40 -0700

    WIP on metric tests.

---
+++ packages/metrics/src/Metric/Metric.test.ts
@@ -0,0 +1,105 @@
+import { MetricDefinition } from "packages/metrics/dist/Metric/Metric";
+import { Metric } from "./Metric";
+import { MetricLevelSet } from "./MetricLevel";
+
+///// Test data used within tests /////
+
+// Example of a typical MetricLevelSet with id "default" to be used by metrics
+// that don't specify a non-default one.
+const testLevelSet: MetricLevelSet = {
+  id: "default",
+  defaultLevel: { id: "unknown", color: "grey" },
+  levels: [
+    { id: "low", color: "green" },
+    { id: "medium", color: "yellow" },
+    { id: "high", color: "red" },
+  ],
+};
+
+// Example of a typical metric with mostly default options.
+const testMetricDef: MetricDefinition = {
+  id: "cases",
+  name: "Cases per 100k",
+};
+
+describe("Metric", () => {
+  test("constructor smoke test", () => {
+    const metric = new Metric(testMetricDef, [testLevelSet]);
+    expect(metric.id).toBe("cases");
+    expect(metric.name).toBe("Cases per 100k");
+  });
+
+  test("constructor resolves levelSetId to correct levelSet", () => {
+    const customLevelSet = { ...testLevelSet, id: "custom" };
+    const levelSets = [testLevelSet, customLevelSet];
+
+    const metric = new Metric(testMetricDef, levelSets);
+    expect(metric.levelSet).toBe(testLevelSet);
+
+    // Test with custom levelSetId.
+    const metric2Def = { ...testMetricDef, levelSetId: "custom" };
+    const metric2 = new Metric(metric2Def, levelSets);
+    expect(metric2.levelSet).toBe(customLevelSet);
+  });
+
+  test("getLevel() grades values using thresholds", () => {
+    const metric = new Metric(
+      {
+        ...testMetricDef,
+        thresholds: [10, 20],
+      },
+      [testLevelSet]
+    );
+    expect(metric.getLevel(5).id).toBe("low");
+    expect(metric.getLevel(15).id).toBe("medium");
+    expect(metric.getLevel(25).id).toBe("high");
+
+    // threshold values should err towards the lower level.
+    expect(metric.getLevel(10).id).toBe("low");
+    expect(metric.getLevel(20).id).toBe("medium");
+  });
+
+  test("getLevel() grades values using descending thresholds", () => {
+    const metric = new Metric(
+      {
+        ...testMetricDef,
+        thresholds: [20, 10],
+      },
+      [testLevelSet]
+    );
+    expect(metric.getLevel(5).id).toBe("high");
+    expect(metric.getLevel(15).id).toBe("medium");
+    expect(metric.getLevel(25).id).toBe("low");
+
+    // threshold values should err towards the lower level.
+    expect(metric.getLevel(10).id).toBe("medium");
+    expect(metric.getLevel(20).id).toBe("low");
+  });
+
+  test("formatValue() with default options", () => {
+    const metric = new Metric(testMetricDef);
+    expect(metric.formatValue(123.123)).toBe("123.1");
+  });
+
+  test("formatValue() with custom options", () => {
+    const metric = new Metric({
+      ...testMetricDef,
+      formatOptions: { style: "percent", maximumFractionDigits: 0 },
+    });
+    // Percent metrics are expected to be 0-1 ratios.
+    expect(metric.formatValue(0.1234)).toBe("12%");
+  });
+
+  test("roundValue() with default options", () => {
+    const metric = new Metric(testMetricDef);
+    expect(metric.roundValue(123.15)).toBe(123.2);
+  });
+
+  test("roundValue() with custom options", () => {
+    const metric = new Metric({
+      ...testMetricDef,
+      formatOptions: { style: "percent", maximumFractionDigits: 0 },
+    });
+    expect(metric.roundValue(0.123)).toBe(0.12);
+  });
+});
