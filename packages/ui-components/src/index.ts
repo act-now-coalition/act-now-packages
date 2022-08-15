316fece7735faebe80b5d1bbbdd15ef6a5ce22ab
Pablo Navarro <pnavarrc@gmail.com>
2022-08-15 09:02:33 -0700

    Implements LineChart (#50)

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -8,6 +8,8 @@ export { default as LegendThreshold } from "./components/LegendThreshold";
 export type { LegendThresholdProps } from "./components/LegendThreshold";
 export { default as LegendCategorical } from "./components/LegendCategorical";
 export type { LegendCategoricalProps } from "./components/LegendCategorical";
+export { default as LineChart } from "./components/LineChart";
+export type { LineChartProps } from "./components/LineChart";
 export { default as ProgressBar } from "./components/ProgressBar";
 export type { ProgressBarProps } from "./components/ProgressBar";
 export { default as RegionSearch } from "./components/RegionSearch";
