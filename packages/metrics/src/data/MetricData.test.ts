2ee5c1489a275822af1c181785f1e11b4efd2aaa
Sean McClure <sean.mcclure17@gmail.com>
2022-08-16 15:40:03 -0400

    Add tests for assertBoolean and convertToBoolean

---
+++ packages/metrics/src/data/MetricData.test.ts
@@ -0,0 +1,41 @@
+import { states } from "@actnowcoalition/regions";
+import { Metric } from "../Metric";
+import { Timeseries } from "../Timeseries";
+import { MetricData } from "./MetricData";
+
+const testRegion = states.findByRegionIdStrict("36");
+const testMetric = new Metric({ id: "boolean_metric" });
+const buildMetricData = (value: unknown) => {
+  return new MetricData(
+    testMetric,
+    testRegion,
+    value,
+    Timeseries.fromJSON([{ date: "2022-08-01", value: value }])
+  );
+};
+
+describe("MetricData", () => {
+  test("assertBoolean() succeeds on boolean data.", () => {
+    expect(buildMetricData(true).assertBoolean()).toStrictEqual(
+      buildMetricData(true)
+    );
+  });
+
+  test("assertBoolean() fails on non-boolean data.", () => {
+    expect(() => {
+      buildMetricData("not boolean").assertBoolean();
+    }).toThrow();
+  });
+
+  test("convertToBoolean() succeeds on near-boolean data.", () => {
+    expect(buildMetricData("yes").convertToBoolean()).toStrictEqual(
+      buildMetricData(true)
+    );
+  });
+
+  test("convertToBoolean() fails on non-boolean data.", () => {
+    expect(() => {
+      buildMetricData("not boolean").convertToBoolean();
+    }).toThrow();
+  });
+});
