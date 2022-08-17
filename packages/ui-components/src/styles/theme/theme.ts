12bfd9634df2497e1a74dfae7284779ee1b15dcb
chasulin <44076375+chasulin@users.noreply.github.com>
2022-08-17 12:13:36 -0400

    ui components: InfoTooltip (#67)

--- packages/ui-components/src/styles/theme/theme.ts
+++ packages/ui-components/src/styles/theme/theme.ts
@@ -1,7 +1,7 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import palette from "./palette";
 import typography from "./typography";
-
+import components from "./components";
 /**
  * Theme configuration variables
  * https://mui.com/customization/theming/#theme-configuration-variables
@@ -10,6 +10,7 @@ import typography from "./typography";
 const themeConfig = {
   palette,
   typography,
+  components,
 };
 
 const theme = responsiveFontSizes(createTheme(themeConfig));
