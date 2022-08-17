26a1258aaac014eea9f2fed4ef2a0b25f11c39bd
Michael Lehenbauer <mikelehen@gmail.com>
2022-08-17 16:50:37 -0700

    Fix useData() hooks and add testing.
    The existing useData() hooks didn't gracefully handle passing the same metrics / regions on every
    render if the actual array instances changed, e.g.: This would cause an infinite re-render:
    
    catalog.useDataForRegionsAndMetrics([region], [metric])
    
    Because the array instances would be differente every time.  The core fix is to add a
    `useCachedArrayIfEqual()` helper that preserves the original array reference if the contents are the
    same.

--- packages/metrics/src/MetricCatalog/MetricCatalog.test.ts
+++ packages/metrics/src/MetricCatalog/MetricCatalog.test.ts
@@ -1,3 +1,5 @@
+import { renderHook } from "@testing-library/react-hooks";
+
 import { states } from "@actnowcoalition/regions";
 
 import { MetricCatalog } from "./MetricCatalog";
@@ -146,5 +148,76 @@ describe("MetricCatalog", () => {
     );
     expect(typeof data.currentValue).toEqual("number");
     expect(data.timeseries.hasData()).toEqual(true);
+  });
+
+  test("useDataForRegionsAndMetrics()", async () => {
+    // This test makes sure that the hook gracefully handles being rendered
+    // repeatedly with the same set of metrics / regions, even if the arrays
+    // are not the exact same instances. In particular, each render should
+    // not trigger additional metric fetches.
+
+    const dataProviders = [
+      new StaticValueDataProvider(),
+      new MockDataProvider(),
+    ];
+    const catalog = new MetricCatalog(testMetricDefs, dataProviders);
+    const metricPi = catalog.getMetric(MetricId.PI);
+    const metricCases = catalog.getMetric(MetricId.MOCK_CASES);
+
+    // Before we render the hook, the catalog should not have done any data fetches.
+    let expectedFetches = 0;
+    expect(catalog.dataFetchesCount).toBe(expectedFetches);
+
+    // Render the hook initially.
+    const { result, waitForNextUpdate, rerender } = renderHook(
+      ({ regions, metrics }) =>
+        catalog.useDataForRegionsAndMetrics(regions, metrics),
+      {
+        initialProps: {
+          regions: [testRegionCA, testRegionWA],
+          metrics: [metricPi, metricCases],
+        },
+      }
+    );
+
+    // The catalog should have performed a data fetch, but since it's anync,
+    // initially the hook won't return data.
+    expect(catalog.dataFetchesCount).toBe(++expectedFetches);
+    expect(result.current.data).toStrictEqual(undefined);
+    // Wait for initial async fetch to finish.
+    await waitForNextUpdate();
+    expect(
+      result.current.data?.metricData(testRegionCA, metricPi).currentValue
+    ).toBe(Math.PI);
+
+    // Render the hook again with new regions / metrics array instances but
+    // containing the same values.  This should not perform a fetch.
+    rerender({
+      regions: [testRegionCA, testRegionWA],
+      metrics: [metricPi, metricCases],
+    });
+    expect(catalog.dataFetchesCount).toBe(expectedFetches);
+
+    // Render the hook again with different regions.  This should perform a fetch.
+    rerender({ regions: [testRegionCA], metrics: [metricPi, metricCases] });
+    expect(catalog.dataFetchesCount).toBe(++expectedFetches);
+    // Wait for async fetch to return the new data.
+    await waitForNextUpdate();
+    expect(result.current.data?.hasRegionData(testRegionWA)).toBe(false);
+    expect(
+      result.current.data?.metricData(testRegionCA, metricPi).currentValue
+    ).toBe(Math.PI);
+
+    // Render the hook again with different metrics.  This should perform a fetch.
+    rerender({ regions: [testRegionCA], metrics: [metricPi] });
+    expect(catalog.dataFetchesCount).toBe(++expectedFetches);
+    // Wait for async fetch to return the new data.
+    await waitForNextUpdate();
+    expect(
+      result.current.data?.regionData(testRegionCA).hasMetricData(metricCases)
+    ).toBe(false);
+    expect(
+      result.current.data?.metricData(testRegionCA, metricPi).currentValue
+    ).toBe(Math.PI);
   });
 });
