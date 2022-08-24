c41cb04d6d440f3a86e91a2e2f27babbf0b13395
chasulin <44076375+chasulin@users.noreply.github.com>
2022-08-24 14:15:51 -0400

    add links to scale docs

--- packages/ui-components/src/components/USNationalMap/USNationalMap.stories.tsx
+++ packages/ui-components/src/components/USNationalMap/USNationalMap.stories.tsx
@@ -35,20 +35,23 @@ CountiesWithNoFillColor.args = {
 
 const alphabetArr = "abcdefghijklmnopqrstuvwxyz".split("");
 
-// Linear scale that scales a number between 0-25 into a corresponding number between 0-1
+// Linear scale that scales a number between 0-25 into a corresponding number between 0-1.
+// https://observablehq.com/@d3/d3-scalelinear
 const testLinear = scaleLinear({
   domain: [0, 25],
   range: [0, 1],
 });
 
-// Creates an array of colors (in a continuous color scale) with as many colors as there are letters of the alphabet (26)
-// Documentation re: `interpolatePiYG` - https://github.com/d3/d3-scale-chromatic/blob/main/README.md
+// Creates an array of colors (in a continuous color scale) with
+// as many colors as there are letters of the alphabet (26).
+// Docs re: `interpolatePiYG` - https://github.com/d3/d3-scale-chromatic/blob/main/README.md
 const colors = alphabetArr.map((letter: string, i: number) =>
   interpolatePiYG(testLinear(i))
 );
 
 // Ordinal scale that takes a letter in the alphabet and returns the color from
-// the `colors` array at the index that matches with the letter's index in the alphabet
+// the `colors` array at the index that matches with the letter's index in the alphabet.
+// https://observablehq.com/@d3/d3-scaleordinal
 const colorScaleAlpha = scaleOrdinal({
   domain: alphabetArr,
   range: colors,
