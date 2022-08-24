5ad5f9baa886e0191f95c0d4fb8c2c2524bad3c2
chasulin <44076375+chasulin@users.noreply.github.com>
2022-08-24 14:13:55 -0400

    add comments

--- packages/ui-components/src/components/USNationalMap/USNationalMap.stories.tsx
+++ packages/ui-components/src/components/USNationalMap/USNationalMap.stories.tsx
@@ -33,28 +33,33 @@ CountiesWithNoFillColor.args = {
   renderTooltip: (fips) => renderSimpleTooltip(fips),
 };
 
+const alphabetArr = "abcdefghijklmnopqrstuvwxyz".split("");
+
+// Linear scale that scales a number between 0-25 into a corresponding number between 0-1
+const testLinear = scaleLinear({
+  domain: [0, 25],
+  range: [0, 1],
+});
+
+// Creates an array of colors (in a continuous color scale) with as many colors as there are letters of the alphabet (26)
+// Documentation re: `interpolatePiYG` - https://github.com/d3/d3-scale-chromatic/blob/main/README.md
+const colors = alphabetArr.map((letter: string, i: number) =>
+  interpolatePiYG(testLinear(i))
+);
+
+// Ordinal scale that takes a letter in the alphabet and returns the color from
+// the `colors` array at the index that matches with the letter's index in the alphabet
+const colorScaleAlpha = scaleOrdinal({
+  domain: alphabetArr,
+  range: colors,
+});
+
 const getFillColorByFirstLetter = (fips: string) => {
   const fullNameFromFips = regions
     .findByRegionIdStrict(fips)
     .fullName.toLowerCase();
   return colorScaleAlpha(fullNameFromFips[0]);
 };
-
-const alphabetArr = "abcdefghijklmnopqrstuvwxyz".split("");
-
-const testLinear = scaleLinear({
-  domain: [0, 25],
-  range: [0, 1],
-});
-
-const colors = alphabetArr.map((letter: string, i: number) =>
-  interpolatePiYG(testLinear(i))
-);
-
-const colorScaleAlpha = scaleOrdinal({
-  domain: alphabetArr,
-  range: colors,
-});
 
 /** States colored by first letter of fullName */
 export const StatesColoredByFirstLetter = Template.bind({});
