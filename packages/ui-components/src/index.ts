fe2a57f16ccd5a41d683772b193b6bfdab9b9844
Michael Lehenbauer <mikelehen@gmail.com>
2022-08-30 13:22:21 -0700

    Downgrade d3-geo and export USNationalMap.

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -89,5 +89,4 @@ export { AxisLeft, AxisBottom } from "./components/Axis";
 export type { AxisLeftProps, AxisBottomProps } from "./components/Axis";
 // Note (8/10/22) - Markdown is causing a bug. Commenting it out until fixed.
 // export { default as Markdown } from "./components/Markdown";
-// Note (8/29/22) - Same problem with d3-geo, commenting out until fixed.
-// export { default as USNationalMap } from "./components/USNationalMap";
+export { default as USNationalMap } from "./components/USNationalMap";
