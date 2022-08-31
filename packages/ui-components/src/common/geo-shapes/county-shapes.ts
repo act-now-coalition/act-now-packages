349c574148b1d1defac437c6a37a1d73c657d8bf
chasulin <44076375+chasulin@users.noreply.github.com>
2022-08-31 17:32:12 -0400

    clean up county-shapes file

--- packages/ui-components/src/common/geo-shapes/county-shapes.ts
+++ packages/ui-components/src/common/geo-shapes/county-shapes.ts
@@ -2,11 +2,7 @@ import { Topology } from "topojson-specification";
 import { excludeTerritories, getGeoBorders, getGeoShapes } from "./utils";
 import { ExtendedFeature } from "d3-geo";
 import countiesJSON from "./counties-10m.json";
-import { singletonHook } from "react-singleton-hook";
-import { usePromise } from "../hooks";
-import { importJson } from "../utils";
 
-// Used in new implementation of CountiesMap
 const countiesTopology = countiesJSON as unknown as Topology;
 
 const allCounties = getGeoShapes(countiesTopology, "counties");
@@ -19,15 +15,4 @@ export const countyBorders = getGeoBorders(countiesTopology, "counties");
 export const countiesGeographies = excludeTerritories(
   allCounties,
   getCountyGeoId
-);
-
-// Used in original implementation of CountiesMap
-export type CountiesTopology = typeof import("./counties-10m.json");
-
-function importCountyGeographies(): Promise<CountiesTopology> {
-  return importJson("counties-10m", import("./counties-10m.json"));
-}
-
-export const useCountyGeographies = singletonHook({ pending: true }, () =>
-  usePromise(importCountyGeographies())
 );
