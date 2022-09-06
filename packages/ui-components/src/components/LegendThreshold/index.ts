2aa5a8961cb964ff286edc746304400f98cd5fab
Sean McClure <sean.mcclure17@gmail.com>
2022-09-06 17:20:25 -0400

    use LegendThreshold

--- packages/ui-components/src/components/LegendThreshold/index.ts
+++ packages/ui-components/src/components/LegendThreshold/index.ts
@@ -1,7 +1,4 @@
 import LegendThreshold from "./LegendThreshold";
-import LegendThresholdHorizontal from "./LegendThresholdHorizontal";
-import LegendThresholdVertical from "./LegendThresholdVertical";
 
 export type { LegendThresholdProps } from "./LegendThreshold";
-export { LegendThresholdHorizontal, LegendThresholdVertical };
 export default LegendThreshold;
--- packages/ui-components/src/components/MetricLegendThreshold/MetricLegendThreshold.tsx
+++ packages/ui-components/src/components/MetricLegendThreshold/MetricLegendThreshold.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  LegendThresholdVertical,
-  LegendThresholdHorizontal,
-} from "../LegendThreshold/";
-
+import LegendThreshold from "../LegendThreshold";
 import { Metric, MetricLevel } from "@actnowcoalition/metrics";
 import { useMetricCatalog } from "../MetricCatalogContext";
 import { assert } from "@actnowcoalition/assert";
@@ -11,17 +7,19 @@ import { Stack, Typography } from "@mui/material";
 
 const getItemColor = (item: MetricLevel) => item.color;
 const getItemLabel = (item: MetricLevel) => item.name ?? item.id;
-const getItemSubLabel = () => "sublabel text";
+const getItemSublabel = (item: MetricLevel) => item.description ?? ""; // TODO: add sub-label/description to MetricLevel
 
 export type MetricLegendThresholdProps = {
   metric: Metric | string;
   orientation: "vertical" | "horizontal";
-  supportingText: string;
+  supportingText?: string;
   height?: number;
   barHeight?: number;
+  barWidth?: number;
   width?: number;
   borderRadius?: number;
   showLabels?: boolean;
+  otherSvgProps?: React.SVGProps<SVGSVGElement>;
 };
 
 const MetricLegendThreshold = (props: MetricLegendThresholdProps) => {
@@ -34,39 +32,25 @@ const MetricLegendThreshold = (props: MetricLegendThresholdProps) => {
       `No thresholds found for metric ${metric.id}.`
   );
 
-  const legendThreshold =
-    props.orientation === "horizontal" ? (
-      <LegendThresholdHorizontal<MetricLevel>
-        {...props}
-        {...{
-          metric,
-          getItemColor: getItemColor,
-          orientation: "horizontal",
-          items: items,
-          getItemLabel: getItemLabel,
-        }}
-      />
-    ) : (
-      <LegendThresholdVertical<MetricLevel>
-        {...props}
-        {...{
-          metric,
-          getItemColor: getItemColor,
-          getItemLabel: getItemLabel,
-          getItemSublabel: getItemSubLabel,
-          orientation: "vertical",
-          items: items,
-        }}
-      />
-    );
-
   return (
     <Stack spacing={props.orientation === "horizontal" ? 2 : 3}>
       <Stack spacing={0.5}>
         <Typography variant="labelLarge">{metric.name}</Typography>
         <Typography variant="paragraphSmall">{props.supportingText}</Typography>
       </Stack>
-      {legendThreshold}
+      <LegendThreshold<MetricLevel>
+        orientation={props.orientation}
+        items={items}
+        getItemColor={getItemColor}
+        getItemLabel={getItemLabel}
+        getItemSublabel={getItemSublabel}
+        height={props.height}
+        barHeight={props.barHeight}
+        barWidth={props.barWidth}
+        width={props.width}
+        borderRadius={props.borderRadius}
+        showLabels={props.showLabels}
+      />
     </Stack>
   );
 };
