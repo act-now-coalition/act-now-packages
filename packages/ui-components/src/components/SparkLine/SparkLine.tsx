ab370242d95a6a0b1723ff72855d87fb48874f61
Fai <fai@covidactnow.org>
2022-09-07 14:55:11 -0700

    Address comments

--- packages/ui-components/src/components/SparkLine/SparkLine.tsx
+++ packages/ui-components/src/components/SparkLine/SparkLine.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Group } from "@visx/group";
 import { Timeseries } from "@actnowcoalition/metrics";
+import { BarChart } from "../BarChart";
 import { LineChart } from "../LineChart";
 import { scaleUtc, scaleLinear } from "@visx/scale";
 import { useTheme } from "@mui/material";
 
-export interface SparkLineOwnProps {
+export interface SparkLineProps {
   /** Timeseries used to draw the bar chart */
   timeseriesBarChart: Timeseries<number>;
 
@@ -22,15 +23,12 @@ export interface SparkLineOwnProps {
   barWidth?: number;
 }
 
-export type SparkLineProps = SparkLineOwnProps & React.SVGProps<SVGRectElement>;
-
 export const SparkLine: React.FC<SparkLineProps> = ({
   timeseriesBarChart,
   timeseriesLineChart,
   width = 150,
   height = 50,
   barWidth = 2,
-  ...rectProps
 }) => {
   const theme = useTheme();
   const padding = 2;
@@ -41,49 +39,41 @@ export const SparkLine: React.FC<SparkLineProps> = ({
 
   const xScaleBar = scaleUtc({
     domain: [timeseriesBarChart.minDate, timeseriesBarChart.maxDate],
-    range: [0, width - padding],
+    range: [0, width - 2 * padding],
   });
 
   const yScaleBar = scaleLinear({
     domain: [timeseriesBarChart.minValue, timeseriesBarChart.maxValue],
-    range: [height - padding, 0],
+    range: [height - 2 * padding, 0],
   });
-
-  const [yStart] = yScaleBar.range();
 
   const xScaleLine = scaleUtc({
     domain: [timeseriesLineChart.minDate, timeseriesLineChart.maxDate],
-    range: [0, width - padding],
+    range: [0, width - 2 * padding],
   });
 
   const yScaleLine = scaleLinear({
     domain: [timeseriesLineChart.minValue, timeseriesLineChart.maxValue],
-    range: [height - padding, 0],
+    range: [height - 2 * padding, 0],
   });
 
   return (
     <svg width={width} height={height}>
-      <Group left={-0.5 * barWidth}>
-        {timeseriesBarChart.points.map((p, i) => {
-          const rectY = yScaleBar(p.value);
-          return (
-            <rect
-              key={`bar-${i}`}
-              x={xScaleBar(p.date)}
-              y={Math.min(rectY, yStart)}
-              width={barWidth}
-              height={Math.abs(rectY - yStart)}
-              fill={theme.palette.border.default}
-              {...rectProps}
-            />
-          );
-        })}
+      <Group left={padding} top={padding}>
+        <Group left={-0.5 * barWidth}>
+          <BarChart
+            timeseries={timeseriesBarChart}
+            xScale={xScaleBar}
+            yScale={yScaleBar}
+            fill={theme.palette.border.default}
+          />
+        </Group>
+        <LineChart
+          timeseries={timeseriesLineChart}
+          xScale={xScaleLine}
+          yScale={yScaleLine}
+        />
       </Group>
-      <LineChart
-        timeseries={timeseriesLineChart}
-        xScale={xScaleLine}
-        yScale={yScaleLine}
-      />
     </svg>
   );
 };
