9a17b022c1b6138a8f3f95f36b08f6b834d0e477
Pablo Navarro <pnavarrc@gmail.com>
2022-09-14 13:41:21 -0700

    Centralize mock timeseries for Storybook

--- packages/ui-components/src/components/BarChart/BarChart.stories.tsx
+++ packages/ui-components/src/components/BarChart/BarChart.stories.tsx
@@ -2,11 +2,8 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { scaleLinear, scaleUtc } from "@visx/scale";
 import { Group } from "@visx/group";
-import { appleStock } from "@visx/mock-data";
-
 import { assert } from "@actnowcoalition/assert";
-import { Timeseries, TimeseriesPoint } from "@actnowcoalition/metrics";
-
+import { appleStockTimeseries } from "../../stories/mockData";
 import { LineChart } from "../LineChart";
 import { BarChart } from ".";
 
@@ -30,16 +27,7 @@ const Template: ComponentStory<typeof BarChart> = (args) => (
   </svg>
 );
 
-// We format the points from appleStock to match TimeseriesPoint<number>
-// so we can use them to initialize Timeseries.
-const points = appleStock.map(
-  (p: { date: string; close: number }): TimeseriesPoint<number> => ({
-    date: new Date(`${p.date.replace(/T.*/, "")}`),
-    value: p.close,
-  })
-);
-
-const timeseries = new Timeseries(points).filterToDateRange({
+const timeseries = appleStockTimeseries.filterToDateRange({
   startAt: new Date("2008-01-01"),
   endAt: new Date("2008-01-31"),
 });
