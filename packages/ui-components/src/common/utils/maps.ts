016d7523c4dcfc9e3a00e4533453d262b4b99e54
chasulin <44076375+chasulin@users.noreply.github.com>
2022-09-19 14:06:03 -0400

    delete now unused code

--- packages/ui-components/src/common/utils/maps.ts
+++ packages/ui-components/src/common/utils/maps.ts
@@ -1,9 +1,4 @@
 import { counties, Region } from "@actnowcoalition/regions";
-import {
-  Metric,
-  MultiRegionMultiMetricDataStore,
-} from "@actnowcoalition/metrics";
-import keyBy from "lodash/keyBy";
 
 /** Checks if a county or congressional district belongs to a given state */
 export function belongsToState(
@@ -20,24 +15,4 @@ export function getCountiesOfState(stateRegionId: string): Region[] {
   return counties.all.filter(
     (county) => county.parent?.regionId === stateRegionId
   );
-}
-
-interface FillColorForRegionId {
-  [regionId: string]: {
-    regionId: string;
-    fillColor: string;
-  };
-}
-
-export function generateFillColorMap(
-  regions: Region[],
-  metric: string | Metric,
-  data: MultiRegionMultiMetricDataStore
-): FillColorForRegionId {
-  const regionIdAndFillColor = regions.map((region) => ({
-    regionId: region.regionId,
-    fillColor: data.metricData(region, metric).getColor(),
-  }));
-
-  return keyBy(regionIdAndFillColor, (item) => item.regionId);
 }
