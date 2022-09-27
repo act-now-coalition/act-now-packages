003e2054cc2836969fb4101e0913d070842ad0fa
Pablo Navarro <pnavarrc@gmail.com>
2022-09-26 17:54:15 -0700

    combine imports

--- packages/ui-components/src/components/Maps/USNationalMap/USCountiesMap.tsx
+++ packages/ui-components/src/components/Maps/USNationalMap/USCountiesMap.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { ExtendedFeature, GeoProjection, geoPath as d3GeoPath } from "d3-geo";
 import { StyledCanvas } from "../Maps.style";
-import { stateBorders } from "../../../common/geo-shapes";
-import { countiesGeographies } from "../../../common/geo-shapes";
+import { countiesGeographies, stateBorders } from "../../../common/geo-shapes";
 
 const borderColor = "white";
 
