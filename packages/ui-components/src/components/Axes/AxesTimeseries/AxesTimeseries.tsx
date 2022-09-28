518c3a52052541c082c82cd19f5f463be65a8e8d
chasulin <44076375+chasulin@users.noreply.github.com>
2022-09-28 13:56:50 -0400

    delete utils and custom time tick settings

--- packages/ui-components/src/components/Axes/AxesTimeseries/AxesTimeseries.tsx
+++ packages/ui-components/src/components/Axes/AxesTimeseries/AxesTimeseries.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { AxisLeft, AxisBottom } from "../../Axis/Axis.style";
 import { ScaleTime, ScaleLinear } from "d3-scale";
-import { TimeUnit } from "@actnowcoalition/time-utils";
-import { getTimeAxisTicks, getXTickFormat } from "../../../common/utils/charts";
 
 export interface AxesTimeseriesProps {
   height: number;
   dateScale: ScaleTime<number, number>;
   yScale: ScaleLinear<number, number>;
   yNumTicks?: number;
-  xTickTimeUnit?: TimeUnit;
 }
 
 export const AxesTimeseries: React.FC<AxesTimeseriesProps> = ({
@@ -17,20 +14,11 @@ export const AxesTimeseries: React.FC<AxesTimeseriesProps> = ({
   dateScale,
   yScale,
   yNumTicks = 10,
-  xTickTimeUnit = TimeUnit.MONTHS,
 }) => {
-  const [dateFrom, dateTo] = dateScale.domain();
-  const timeTicks = getTimeAxisTicks(dateFrom, dateTo, xTickTimeUnit);
-
   return (
     <>
       <AxisLeft scale={yScale} numTicks={yNumTicks} />
-      <AxisBottom
-        top={height}
-        scale={dateScale}
-        tickValues={timeTicks}
-        tickFormat={(date: Date) => getXTickFormat(date, xTickTimeUnit)}
-      />
+      <AxisBottom top={height} scale={dateScale} />
     </>
   );
 };
