5be6a8b8fed2e3525561867dd79cf6c8c3f71f05
Pablo Navarro <pnavarrc@gmail.com>
2022-09-28 17:27:28 -0700

    Update the docstrings for MetricTooltip

--- packages/ui-components/src/components/MetricTooltip/MetricTooltip.tsx
+++ packages/ui-components/src/components/MetricTooltip/MetricTooltip.tsx
@@ -5,6 +5,7 @@ import { Region } from "@actnowcoalition/regions";
 import { formatDateTime, DateFormat } from "@actnowcoalition/time-utils";
 
 export interface MetricTooltipProps extends MetricTooltipContentProps {
+  /** Children is the component that, when hovered, should open the tooltip */
   children: React.ReactNode;
 }
 
@@ -13,21 +14,26 @@ export const MetricTooltip = ({
   region,
   point,
   ...tooltipProps
-}: MetricTooltipProps & Omit<TooltipProps, "title">) => {
-  return (
-    <Tooltip
-      arrow
-      title={
-        <MetricTooltipContent metric={metric} region={region} point={point} />
-      }
-      {...tooltipProps}
-    />
-  );
-};
+}: MetricTooltipProps & Omit<TooltipProps, "title">) => (
+  <Tooltip
+    arrow
+    title={
+      <MetricTooltipContent metric={metric} region={region} point={point} />
+    }
+    {...tooltipProps}
+  />
+);
 
 export interface MetricTooltipContentProps {
+  /** Metric to use to render the tooltip */
   metric: Metric;
+  /** Region to use to render the tooltip */
   region: Region;
+  /**
+   * Point with the date and value to show in the tooltip. The date and
+   * value of the point usually correspond to the point being hovered
+   * by the user. See `ChartOverlayX`, for example.
+   */
   point: TimeseriesPoint<number>;
 }
 
