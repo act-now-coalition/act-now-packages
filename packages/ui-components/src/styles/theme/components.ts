7b27861376ba79d178958e61de1dbea617c2be40
chasulin <44076375+chasulin@users.noreply.github.com>
2022-10-05 15:25:47 -0400

    alphabetize components

--- packages/ui-components/src/styles/theme/components.ts
+++ packages/ui-components/src/styles/theme/components.ts
@@ -5,6 +5,73 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 const referenceTheme = createTheme();
 
 const components: ThemeOptions["components"] = {
+  MuiButton: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        textTransform: "none",
+        fontFamily: theme.typography.labelLarge.fontFamily,
+        fontSize: theme.typography.labelLarge.fontSize,
+        fontWeight: theme.typography.labelLarge.fontWeight,
+        lineHeight: theme.typography.labelLarge.lineHeight,
+      }),
+      outlinedPrimary: ({ theme }) => ({
+        borderColor: theme.palette.border.default,
+      }),
+    },
+  },
+
+  MuiChip: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        borderColor: theme.palette.border.default,
+      }),
+      label: ({ theme }) => ({
+        ...theme.typography.labelSmall,
+        color: theme.palette.secondary.dark,
+      }),
+      deleteIcon: ({ theme }) => ({
+        color: theme.palette.text.disabled,
+        ":hover": {
+          color: theme.palette.secondary.dark,
+        },
+      }),
+    },
+  },
+
+  MuiTab: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        textTransform: "none",
+        padding: `0 0 ${theme.spacing(2)} 0`,
+        margin: `0 ${theme.spacing(2)}`,
+        ":first-of-type": {
+          marginLeft: "0",
+        },
+        ":last-of-type": {
+          marginRight: "0",
+        },
+        ":not(&.Mui-selected)": {
+          span: {
+            color: theme.palette.text.secondary,
+          },
+        },
+      }),
+    },
+  },
+
+  MuiTabs: {
+    styleOverrides: {
+      indicator: ({ theme }) => ({
+        height: "2px",
+        backgroundColor: theme.palette.common.black,
+      }),
+      flexContainer: ({ theme }) => ({
+        borderBottom: `1px solid ${theme.palette.border.default}`,
+        width: "fit-content",
+      }),
+    },
+  },
+
   MuiTextField: {
     defaultProps: {
       SelectProps: {
@@ -38,72 +105,7 @@ const components: ThemeOptions["components"] = {
       }),
     },
   },
-  MuiTabs: {
-    styleOverrides: {
-      indicator: ({ theme }) => ({
-        height: "2px",
-        backgroundColor: theme.palette.common.black,
-      }),
-      flexContainer: ({ theme }) => ({
-        borderBottom: `1px solid ${theme.palette.border.default}`,
-        width: "fit-content",
-      }),
-    },
-  },
-  MuiTab: {
-    styleOverrides: {
-      root: ({ theme }) => ({
-        textTransform: "none",
-        padding: `0 0 ${theme.spacing(2)} 0`,
-        margin: `0 ${theme.spacing(2)}`,
-        ":first-of-type": {
-          marginLeft: "0",
-        },
-        ":last-of-type": {
-          marginRight: "0",
-        },
-        ":not(&.Mui-selected)": {
-          span: {
-            color: theme.palette.text.secondary,
-          },
-        },
-      }),
-    },
-  },
-  MuiButton: {
-    styleOverrides: {
-      root: ({ theme }) => ({
-        textTransform: "none",
-        fontFamily: theme.typography.labelLarge.fontFamily,
-        fontSize: theme.typography.labelLarge.fontSize,
-        fontWeight: theme.typography.labelLarge.fontWeight,
-        lineHeight: theme.typography.labelLarge.lineHeight,
-      }),
-      outlinedPrimary: ({ theme }) => ({
-        borderColor: theme.palette.border.default,
-      }),
-    },
-  },
-  MuiTooltip: {
-    styleOverrides: {
-      tooltip: ({ theme }) => ({
-        backgroundColor: "black",
-        color: "white",
-        fontSize: theme.typography.paragraphLarge.fontSize,
-        lineHeight: theme.typography.paragraphLarge.lineHeight,
-        padding: theme.spacing(2),
-        "& a": {
-          color: "white",
-        },
-        [referenceTheme.breakpoints.down("sm")]: {
-          padding: "20px 24px",
-        },
-      }),
-      arrow: {
-        color: "black",
-      },
-    },
-  },
+
   MuiToggleButtonGroup: {
     styleOverrides: {
       root: ({ theme }) => ({
@@ -145,21 +147,25 @@ const components: ThemeOptions["components"] = {
       }),
     },
   },
-  MuiChip: {
+
+  MuiTooltip: {
     styleOverrides: {
-      root: ({ theme }) => ({
-        borderColor: theme.palette.border.default,
-      }),
-      label: ({ theme }) => ({
-        ...theme.typography.labelSmall,
-        color: theme.palette.secondary.dark,
-      }),
-      deleteIcon: ({ theme }) => ({
-        color: theme.palette.text.disabled,
-        ":hover": {
-          color: theme.palette.secondary.dark,
+      tooltip: ({ theme }) => ({
+        backgroundColor: "black",
+        color: "white",
+        fontSize: theme.typography.paragraphLarge.fontSize,
+        lineHeight: theme.typography.paragraphLarge.lineHeight,
+        padding: theme.spacing(2),
+        "& a": {
+          color: "white",
+        },
+        [referenceTheme.breakpoints.down("sm")]: {
+          padding: "20px 24px",
         },
       }),
+      arrow: {
+        color: "black",
+      },
     },
   },
 };
