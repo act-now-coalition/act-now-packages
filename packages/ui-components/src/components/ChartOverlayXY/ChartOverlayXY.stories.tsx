452d75f9d4ba7e5751653e546e0ba6ab1db38a1b
Pablo Navarro <pnavarrc@gmail.com>
2022-10-11 09:58:42 -0700

    Update documentation of the component

--- packages/ui-components/src/components/ChartOverlayXY/ChartOverlayXY.stories.tsx
+++ packages/ui-components/src/components/ChartOverlayXY/ChartOverlayXY.stories.tsx
@@ -26,6 +26,9 @@ const dates = [
   new Date("2022-01-04"),
   new Date("2022-01-05"),
   new Date("2022-01-06"),
+  new Date("2022-01-07"),
+  new Date("2022-01-08"),
+  new Date("2022-01-09"),
 ];
 
 function generateSeries(dates: Date[], maxValue: number): Timeseries<number> {
@@ -35,12 +38,6 @@ function generateSeries(dates: Date[], maxValue: number): Timeseries<number> {
   }));
   return new Timeseries<number>(points);
 }
-
-const seriesList = [
-  { timeseries: generateSeries(dates, 10), color: "#2196f3" },
-  { timeseries: generateSeries(dates, 20), color: "#66bb6a" },
-  { timeseries: generateSeries(dates, 30), color: "#fb8c00" },
-];
 
 const xScale = scaleUtc({
   domain: [dates[0], dates[dates.length - 1]],
@@ -52,7 +49,21 @@ const yScale = scaleLinear({
   range: [innerHeight, 0],
 });
 
-export const Example = () => {
+const seriesList = [
+  { timeseries: generateSeries(dates, 10), color: "#2196f3" },
+  { timeseries: generateSeries(dates, 20), color: "#66bb6a" },
+  { timeseries: generateSeries(dates, 30), color: "#fb8c00" },
+];
+
+/**
+ * This chart is a demo of a simplified Trends chart. When the user hovers
+ * near a point on one of the series, the entire line is highlighted.
+ *
+ * When the user hovers near a point, the `ChartOverlayXY` component calls the
+ * onMouseMove handler passing the index of the point in the series, and
+ * the index of the series. We use that information to make the line thicker.
+ */
+export const ExampleTrendsChart = () => {
   const [pointIndex, setPointIndex] = useState<number | null>(null);
   const [timeseriesIndex, setTimeseriesIndex] = useState<number | null>(null);
 
@@ -77,31 +88,28 @@ export const Example = () => {
         <AxisLeft scale={yScale} />
         {seriesList.map(({ timeseries, color }, tIndex) => (
           <Group key={`group-${tIndex}`}>
-            {timeseries.points.map((p, pIndex) => (
-              <circle
-                key={`point-${pIndex}`}
-                r={
-                  isHoveringPoint &&
-                  pointIndex === pIndex &&
-                  tIndex === timeseriesIndex
-                    ? 5
-                    : 2
-                }
-                cx={xScale(p.date)}
-                cy={yScale(p.value)}
-                fill={color}
-              />
-            ))}
+            <LineChart
+              timeseries={timeseries}
+              xScale={xScale}
+              yScale={yScale}
+              stroke={color}
+              strokeWidth={
+                isHoveringPoint && tIndex === timeseriesIndex ? 3 : 1
+              }
+            />
+            <Group>
+              {timeseries.points.map((p, pIndex) => (
+                <circle
+                  key={`point-${pIndex}`}
+                  r={3}
+                  cx={xScale(p.date)}
+                  cy={yScale(p.value)}
+                  fill={color}
+                />
+              ))}
+            </Group>
           </Group>
         ))}
-        {isHoveringPoint && (
-          <LineChart
-            timeseries={seriesList[timeseriesIndex].timeseries}
-            xScale={xScale}
-            yScale={yScale}
-            stroke={seriesList[timeseriesIndex].color}
-          />
-        )}
         <ChartOverlayXY
           timeseriesList={seriesList.map((s) => s.timeseries)}
           yScale={yScale}
--- packages/ui-components/src/components/ChartOverlayXY/ChartOverlayXY.tsx
+++ packages/ui-components/src/components/ChartOverlayXY/ChartOverlayXY.tsx
@@ -6,26 +6,32 @@ import { voronoi, VoronoiPolygon } from "@visx/voronoi";
 import { Timeseries, TimeseriesPoint } from "@actnowcoalition/metrics";
 
 export interface ChartOverlayXYProps {
-  /** List of timeseries */
+  /**
+   * List of timeseries objects that contain the hoverable points. The component
+   * receives a list of timeseries to make it easier to know which series was
+   * hovered (not only which point).
+   **/
   timeseriesList: Timeseries<number>[];
-  /** Width */
+  /** Width of the hoverable area. */
   width: number;
-  /** Height */
+  /** Height of the hoverable area. */
   height: number;
-  /** xScale */
+  /** d3-scale to convert between dates and pixel dimensions. */
   xScale: ScaleTime<number, number>;
-  /** yScale */
+  /** d3-scale to convert between data units on the y-axis and pixel dimensions. */
   yScale: ScaleLinear<number, number>;
+  /**
+   * Handler to call when a user hovers near a point. The point, timeseriesIndex
+   * and pointIndex are passed to the handler.
+   **/
   onMouseMove?: (pointInfo: PointInfo) => void;
+  /** Handler to call when the user moves out of the hoverable area. */
   onMouseOut?: () => void;
 }
 
-export interface HoverData {
-  timeseries: Timeseries<number>;
-  point: TimeseriesPoint<number>;
-}
-
-/** PointInfo */
+/**
+ * The PointInfo contains information about the point being hovered.
+ */
 export interface PointInfo {
   /** Hovered point */
   point: TimeseriesPoint<number>;
@@ -36,7 +42,13 @@ export interface PointInfo {
 }
 
 /**
- * Overlay to capture mouse over a point and the neighboring region
+ * This component is an overlay that helps capture the information of the
+ * point that is being hovered by the user. Given a list of timeseries,
+ * the `onMouseMove` handler will be called with the point, index of the
+ * timeseries (`timeseriesIndex`) and index of the point in the timeseries
+ * (`pointIndex`), which can be stored in a state by the parent component.
+ *
+ * See `ChartOverlayXY.stories.tsx` for usage examples.
  */
 export const ChartOverlayXY = ({
   width,
@@ -58,7 +70,8 @@ export const ChartOverlayXY = ({
   // and height. Each point has a corresponding polygon such that all the
   // points in the polygon are closer to its point than to any other
   // point.
-  // Each polygon has the pointInfo attached to it
+  // Each polygon has the pointInfo attached to it, so we can pass it to the
+  // handler when the user hovers it.
   const voronoiPolygons = useMemo(() => {
     const voroniLayout = voronoi<PointInfo>({
       x: ({ point }: PointInfo) => xScale(point.date),
@@ -84,7 +97,10 @@ export const ChartOverlayXY = ({
   );
 };
 
-// Generates identifiable points from a list of timeseries
+/**
+ * Given a list of timeseries, it returns a single list with the information
+ * of each point (point, timeseriesIndex and pointIndex in the timeseries).
+ */
 function getTimeseriesPoints(
   timeseriesList: Timeseries<number>[]
 ): PointInfo[] {
