4b9abecc5ea29d26f15056dcf074cb694ef18976
chasulin <44076375+chasulin@users.noreply.github.com>
2022-10-17 13:55:48 -0400

    Improvements to MultiProgressBar (and metric-aware version) (#296)

--- packages/ui-components/src/components/MultiProgressBar/interfaces.ts
+++ packages/ui-components/src/components/MultiProgressBar/interfaces.ts
@@ -1,14 +1,16 @@
 export interface BaseMultiProgressBarProps {
   maxValue: number;
+  /** Color of charted bars */
+  barColor?: string;
+  /** Background color of progress bar */
+  bgColor?: string;
   width?: number;
   height?: number;
-  bgColor?: string;
   borderRadius?: number;
 }
 
 export interface MultiProgressBarProps<T> extends BaseMultiProgressBarProps {
-  items: T[];
-  getItemColor: (item: T) => string;
+  items: [T, T];
   getItemLabel: (item: T) => string;
   getItemValue: (item: T) => number;
 }
