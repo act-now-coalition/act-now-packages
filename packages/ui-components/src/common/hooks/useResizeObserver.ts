eda52de63e8c65cec149013db749d46cfd79e854
Sean McClure <sean.mcclure17@gmail.com>
2022-10-18 12:56:26 -0400

    Add resize observer hook

---
+++ packages/ui-components/src/common/hooks/useResizeObserver.ts
@@ -0,0 +1,28 @@
+/** Helpers and React Hooks relating to share images and share links. */
+import { useEffect, useState, useCallback, useRef } from "react";
+
+/** Hook to track element resizing using the ResizeObserver API
+ *
+ * Interpolated from: https://tobbelindstrom.com/blog/resize-observer-hook/
+ */
+export const useResizeObserver = () => {
+  const [observerEntry, setObserverEntry] =
+    useState<ResizeObserverEntry | null>(null);
+  const [node, setNode] = useState<HTMLElement | null>(null);
+  const observer = useRef<ResizeObserver | null>(null);
+  const disconnect = useCallback(() => observer.current?.disconnect(), []);
+
+  const observe = useCallback(() => {
+    observer.current = new ResizeObserver(([entry]) => setObserverEntry(entry));
+    if (node) {
+      observer.current.observe(node);
+    }
+  }, [node]);
+
+  useEffect(() => {
+    observe();
+    return () => disconnect();
+  }, [disconnect, observe]);
+
+  return { ref: setNode, observerEntry: observerEntry };
+};
