16fc6edca033a7049bfea3686215df42770a9e36
Sean McClure <sean.mcclure17@gmail.com>
2022-10-18 17:28:36 -0400

    CR changes

--- packages/ui-components/src/common/hooks/useResizeObserver.ts
+++ packages/ui-components/src/common/hooks/useResizeObserver.ts
@@ -1,16 +1,23 @@
 import { useEffect, useState, useCallback, useRef } from "react";
 
+interface useResizeObserverResult {
+  setNodeState: React.Dispatch<React.SetStateAction<HTMLElement | null>>;
+  observerEntry: ResizeObserverEntry | null;
+}
+
 /** Hook to track element resizing using the ResizeObserver API
  *
  * Interpolated from: https://tobbelindstrom.com/blog/resize-observer-hook/
+ *
+ * @returns State action dispatch to set the element to observe and the element's
+ * resize event data.
  */
-export const useResizeObserver = () => {
+export function useResizeObserver(): useResizeObserverResult {
   const [observerEntry, setObserverEntry] =
     useState<ResizeObserverEntry | null>(null);
   const [node, setNode] = useState<HTMLElement | null>(null);
   const observer = useRef<ResizeObserver | null>(null);
-  const disconnect = useCallback(() => observer.current?.disconnect(), []);
-
+  const disconnect = useCallback(() => observer.current?.disconnect?.(), []);
   const observe = useCallback(() => {
     observer.current = new ResizeObserver(([entry]) => setObserverEntry(entry));
     if (node) {
@@ -23,5 +30,5 @@ export const useResizeObserver = () => {
     return () => disconnect();
   }, [disconnect, observe]);
 
-  return { ref: setNode, observerEntry: observerEntry };
-};
+  return { setNodeState: setNode, observerEntry: observerEntry };
+}
