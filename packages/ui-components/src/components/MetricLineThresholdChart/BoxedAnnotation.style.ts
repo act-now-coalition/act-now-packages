59108849834e872758a75fbdbe39a0060ee42d95
Pablo Navarro <pnavarrc@gmail.com>
2022-10-20 14:59:01 -0700

    Improve the styles of boxed annotation

--- packages/ui-components/src/components/MetricLineThresholdChart/BoxedAnnotation.style.ts
+++ packages/ui-components/src/components/MetricLineThresholdChart/BoxedAnnotation.style.ts
@@ -1,7 +1,10 @@
 import { styled } from "../../styles";
 
+// TODO (Pablo): The font-weight for dataTabular is not readable against
+// a light background.
 export const StyledText = styled("text")`
   font-family: ${({ theme }) => theme.typography.dataTabular.fontFamily};
   font-size: ${({ theme }) => theme.typography.dataTabular.fontSize};
-  font-weight: ${({ theme }) => theme.typography.dataTabular.fontWeight};
+  font-weight: ${({ theme }) =>
+    theme.typography.dataEmphasizedSmall.fontWeight};
 `;
