4e002e0aa6cbc9b2ef220811b29b89f738a4525a
Pablo Navarro <pnavarrc@gmail.com>
2022-11-15 15:47:04 -0800

    Add unit tests for useTableStateManager

---
+++ packages/ui-components/src/components/CompareTable/useTableStateManager.test.ts
@@ -0,0 +1,32 @@
+import { act, renderHook } from "@testing-library/react";
+import { SortDirection } from "./interfaces";
+import { useTableStateManager } from "./useTableStateManager";
+
+describe("useTableStateManager", () => {
+  const initialState = {
+    sortColumnId: "population",
+    sortDirection: SortDirection.ASC,
+  };
+
+  test("initialize state correctly", () => {
+    const { result } = renderHook(() => useTableStateManager(initialState));
+    expect(result.current.sortColumnId).toBe(initialState.sortColumnId);
+    expect(result.current.sortDirection).toBe(initialState.sortDirection);
+    expect(typeof result.current.setSortDirection).toBe("function");
+    expect(typeof result.current.setSortingColumn).toBe("function");
+  });
+
+  test("update sorting column", () => {
+    const { result } = renderHook(() => useTableStateManager(initialState));
+    act(() => result.current.setSortingColumn("name"));
+    expect(result.current.sortColumnId).toBe("name");
+    expect(result.current.sortDirection).toBe(initialState.sortDirection);
+  });
+
+  test("update sort direction", () => {
+    const { result } = renderHook(() => useTableStateManager(initialState));
+    act(() => result.current.setSortDirection(SortDirection.DESC));
+    expect(result.current.sortColumnId).toBe(initialState.sortColumnId);
+    expect(result.current.sortDirection).toBe(SortDirection.DESC);
+  });
+});
