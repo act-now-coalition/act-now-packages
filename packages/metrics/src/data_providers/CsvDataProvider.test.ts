a114c97e0a47278db68a832892ae3127b6b881b6
Michael Lehenbauer <mikelehen@gmail.com>
2022-11-22 08:02:45 -0800

    Route MetricCatalog through SimpleMetricDataProviderBase.

--- packages/metrics/src/data_providers/CsvDataProvider.test.ts
+++ packages/metrics/src/data_providers/CsvDataProvider.test.ts
@@ -1,6 +1,7 @@
 import { CsvDataProvider } from "./CsvDataProvider";
 import { Metric } from "../Metric";
 import { states } from "@actnowcoalition/regions";
+import { MetricCatalog } from "../MetricCatalog";
 
 const PROVIDER_ID = "csv-provider";
 
@@ -39,7 +40,10 @@ const testFetchingCsvData = async (
     dateColumn: dateCol,
     csvText: data,
   });
-  return (await provider.fetchData([newYork], [metric], includeTimeseries))
+  const catalog = new MetricCatalog([metric], [provider]);
+  return (
+    await provider.fetchData([newYork], [metric], includeTimeseries, catalog)
+  )
     .regionData(newYork)
     .metricData(testMetric);
 };
