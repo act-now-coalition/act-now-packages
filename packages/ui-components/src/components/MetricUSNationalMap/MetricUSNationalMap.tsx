fb486421eb701331b607589e18439061a7bfe0ea
Michael Lehenbauer <mikelehen@gmail.com>
2022-11-29 14:26:14 -0800

    Make maps render in grey while loading.

--- packages/ui-components/src/components/MetricUSNationalMap/MetricUSNationalMap.tsx
+++ packages/ui-components/src/components/MetricUSNationalMap/MetricUSNationalMap.tsx
@@ -17,15 +17,13 @@ export const MetricUSNationalMap = ({
 }: MetricUSNationalMapProps) => {
   const { data } = useDataForRegionsAndMetrics(regionDB.all, [metric], false);
 
-  if (!data) {
-    return null;
-  }
-
   return (
     <USNationalMap
       getFillColor={(regionId: string) => {
         const region = regionDB.findByRegionId(regionId);
-        return region ? data.metricData(region, metric).getColor() : "#eee";
+        return region && data
+          ? data.metricData(region, metric).getColor()
+          : "#eee";
       }}
       getRegionUrl={(regionId: string) => {
         const region = regionDB.findByRegionId(regionId);
--- packages/ui-components/src/components/MetricUSStateMap/MetricUSStateMap.tsx
+++ packages/ui-components/src/components/MetricUSStateMap/MetricUSStateMap.tsx
@@ -22,15 +22,13 @@ export const MetricUSStateMap = ({
 
   const { data } = useDataForRegionsAndMetrics(mapRegions, [metric], false);
 
-  if (!data) {
-    return null;
-  }
-
   return (
     <USStateMap
       getFillColor={(regionId: string) => {
         const region = regionDB.findByRegionId(regionId);
-        return region ? data.metricData(region, metric).getColor() : "#eee";
+        return region && data
+          ? data.metricData(region, metric).getColor()
+          : "#eee";
       }}
       getRegionUrl={(regionId: string) => {
         const region = regionDB.findByRegionId(regionId);
