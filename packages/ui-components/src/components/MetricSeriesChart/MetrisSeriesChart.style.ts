3b192534a467160007b0019dc3023673a1ced1f2
Pablo Navarro <pnavarrc@gmail.com>
2022-12-01 16:32:29 -0800

    Fix typography

--- packages/ui-components/src/components/MetricSeriesChart/MetrisSeriesChart.style.ts
+++ packages/ui-components/src/components/MetricSeriesChart/MetrisSeriesChart.style.ts
@@ -3,5 +3,6 @@ import { styled } from "../../styles";
 export const SeriesLabel = styled("text")`
   font-size: ${({ theme }) => theme.typography.paragraphSmall.fontSize};
   font-weight: ${({ theme }) => theme.typography.paragraphSmall.fontWeight};
+  font-family: ${({ theme }) => theme.typography.paragraphSmall.fontFamily};
   dominant-baseline: middle;
 `;
