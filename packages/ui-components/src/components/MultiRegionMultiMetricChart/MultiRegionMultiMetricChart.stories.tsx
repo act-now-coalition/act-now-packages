67920cf662ed0813789d100f32155db6fe0b8ab0
Pablo Navarro <pnavarrc@gmail.com>
2022-12-07 14:02:56 -0800

    Add colors to generated series

--- packages/ui-components/src/components/MultiRegionMultiMetricChart/MultiRegionMultiMetricChart.stories.tsx
+++ packages/ui-components/src/components/MultiRegionMultiMetricChart/MultiRegionMultiMetricChart.stories.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import sortBy from "lodash/sortBy";
 
 import { states } from "@actnowcoalition/regions";
 
 import { MultiRegionMultiMetricChart } from ".";
-import { metricCatalog } from "../../stories/mockMetricCatalog";
+import { MetricId, metricCatalog } from "../../stories/mockMetricCatalog";
+
+const sortedStates = sortBy(states.all, (state) => state.shortName);
 
 export default {
   title: "Components/MultiRegionMultiMetricChart",
@@ -18,8 +21,8 @@ const Template: ComponentStory<typeof MultiRegionMultiMetricChart> = (args) => (
 
 export const Example = Template.bind({});
 Example.args = {
-  regions: states.all,
+  regions: sortedStates,
   metrics: metricCatalog.metrics,
-  initialMetric: metricCatalog.metrics[1],
-  initialRegions: states.all.slice(0, 5),
+  initialMetric: MetricId.MOCK_CASES,
+  initialRegions: sortedStates.slice(0, 5),
 };
--- packages/ui-components/src/components/MultiRegionMultiMetricChart/utils.ts
+++ packages/ui-components/src/components/MultiRegionMultiMetricChart/utils.ts
@@ -1,13 +1,18 @@
+import { schemeCategory10 } from "d3-scale-chromatic";
+
 import { Metric } from "@actnowcoalition/metrics";
 import { Region } from "@actnowcoalition/regions";
 
 import { Series, SeriesType } from "../SeriesChart";
 
 export function getMetricSeries(metric: Metric, regions: Region[]): Series[] {
-  return regions.map((region) => ({
+  return regions.map((region, index) => ({
     metric,
     region,
     type: SeriesType.LINE,
     label: region.shortName,
+    lineProps: {
+      stroke: schemeCategory10[index % schemeCategory10.length],
+    },
   }));
 }
