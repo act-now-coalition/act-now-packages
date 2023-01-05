ff55ea5480b33a0a20858522c98918b8027d323b
Sean McClure <sean.mcclure17@gmail.com>
2023-01-05 13:59:08 -0500

    Add MetricOverviewsBlock

--- packages/ui-components/src/components/MetricOverviewsBlock/MetricOverviewsBlock.stories.tsx
+++ packages/ui-components/src/components/MetricOverviewsBlock/MetricOverviewsBlock.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Box, Typography } from "@mui/material";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 import { states } from "@actnowcoalition/regions";
@@ -13,18 +14,44 @@ export default {
 } as ComponentMeta<typeof MetricOverviewsBlock>;
 
 const Template: ComponentStory<typeof MetricOverviewsBlock> = (args) => (
-  <MetricOverviewsBlock {...args} />
+  <Box sx={{ border: "1px solid #eee" }}>
+    <MetricOverviewsBlock {...args} />
+  </Box>
 );
 
-export const Example = Template.bind({});
-Example.args = {
+const ChartPlaceholder = (name: string) => (
+  <div
+    style={{
+      backgroundColor: "#eee",
+      borderRadius: 4,
+      display: "grid",
+      placeItems: "center",
+      textTransform: "uppercase",
+      minHeight: 72,
+    }}
+  >
+    <Typography variant="overline">{name}</Typography>
+  </div>
+);
+
+const defaultArgs = {
   region: states.findByRegionIdStrict("12"),
   metrics: [
     MetricId.APPLE_STOCK,
     MetricId.MOCK_CASES,
-    MetricId.APPLE_STOCK,
-    MetricId.MOCK_CASES,
-    MetricId.APPLE_STOCK,
-    MetricId.MOCK_CASES,
+    MetricId.NYC_TEMPERATURE,
   ],
+};
+
+export const Example = Template.bind({});
+Example.args = defaultArgs;
+
+export const ExampleWithCharts = Template.bind({});
+ExampleWithCharts.args = {
+  ...defaultArgs,
+  metricCharts: {
+    [MetricId.APPLE_STOCK]: ChartPlaceholder("Apple Stock"),
+    [MetricId.MOCK_CASES]: ChartPlaceholder("Mock Cases"),
+    [MetricId.NYC_TEMPERATURE]: ChartPlaceholder("NYC Temperature"),
+  },
 };
--- packages/ui-components/src/components/MetricOverviewsBlock/MetricOverviewsBlock.tsx
+++ packages/ui-components/src/components/MetricOverviewsBlock/MetricOverviewsBlock.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Stack } from "@mui/material";
+import { Stack, useMediaQuery, useTheme } from "@mui/material";
 
 import { Metric } from "@actnowcoalition/metrics";
 import { Region } from "@actnowcoalition/regions";
@@ -9,22 +9,43 @@ import { useMetricCatalog } from "../MetricCatalogContext";
 import { MetricOverview } from "../MetricOverview";
 
 export interface MetricOverviewsBlockProps {
+  /**
+   * Region represented by the metric overviews.
+   */
   region: Region;
+  /**
+   * Metrics to display overviews for.
+   */
   metrics: Array<Metric | string>;
+  /**
+   * Charts to render for each metric. Charts will not be rendered on small screens.
+   */
+  metricCharts?: { [metricId: string]: React.ReactNode };
 }
 
 export const MetricOverviewsBlock = ({
   region,
   metrics,
+  metricCharts,
 }: MetricOverviewsBlockProps) => {
   const metricCatalog = useMetricCatalog();
   const resolvedMetrics = metrics.map((metric) =>
     metricCatalog.getMetric(metric)
   );
+  const theme = useTheme();
+  const isSmallBreakpoint = useMediaQuery(
+    `(max-width: ${theme.breakpoints.values.sm}px)`
+  );
   return (
-    <Stack direction={"row"} spacing={5}>
+    <Stack direction={isSmallBreakpoint ? "column" : "row"} spacing={5}>
       {resolvedMetrics.map((metric) => (
-        <MetricOverview region={region} metric={metric} key={metric.id} />
+        <MetricOverview
+          region={region}
+          metric={metric}
+          key={metric.id}
+          orientation={isSmallBreakpoint ? "horizontal" : "vertical"}
+          metricChart={metricCharts?.[metric.id]}
+        />
       ))}
     </Stack>
   );
