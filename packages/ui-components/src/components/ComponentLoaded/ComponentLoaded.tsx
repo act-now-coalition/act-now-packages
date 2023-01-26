a5872dfb7a9d9e24e3b149b37961587665f13169
Sean McClure <sean.mcclure17@gmail.com>
2023-01-26 16:30:26 -0500

    cleanup

--- packages/ui-components/src/components/ComponentLoaded/ComponentLoaded.tsx
+++ packages/ui-components/src/components/ComponentLoaded/ComponentLoaded.tsx
@@ -2,6 +2,12 @@ import React from "react";
 
 import { Box } from "@mui/material";
 
+/**
+ * Empty component to be appended to another component to indicate that it has fully loaded.
+ *
+ * Used for share image generation to ensure we wait until the component has loaded
+ * before capturing the image.
+ */
 export const ComponentLoaded = () => {
   return <Box className="component-loaded" style={{ display: "none" }}></Box>;
 };
