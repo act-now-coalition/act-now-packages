fde264a5fe27cda4da60729a1c1dabcb34d1fbfa
Michael Lehenbauer <mikelehen@gmail.com>
2023-01-29 09:39:04 -0800

    Fix storybook.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,6 +13,16 @@ export default {
     // https://storybook.js.org/docs/react/sharing/storybook-composition
     buildStoriesJson: true,
   },
+  babel: async (options: any) => {
+    // NOTE(michael 2023-01-30): By default Storybook adds a bunch of babel
+    // plugins that end up causing syntax errors with some TypeScript features
+    // (in particular constructor properties like `constructor(private foo:
+    // number) { }`). I eventually just tried removing them so we get babel
+    // defaults, and everything worked. Maybe because we're using a recent
+    // version of babel we don't need all of those plugins.
+    options["plugins"] = [];
+    return options;
+  },
   webpackFinal: async (config: any) => {
     // Add "src" alias so we can resolve imports like "src/assert" For
     // TypeScript this is handled by setting baseUrl to "." but Storybook /
