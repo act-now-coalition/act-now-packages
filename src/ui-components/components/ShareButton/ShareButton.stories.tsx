0e69c7c806bd56fbc5a6c215f5cab9e32ad0878c
Pablo Navarro <pnavarrc@gmail.com>
2023-02-07 12:51:51 -0800

    Update prop names and docstrings

--- src/ui-components/components/ShareButton/ShareButton.stories.tsx
+++ src/ui-components/components/ShareButton/ShareButton.stories.tsx
@@ -64,7 +64,7 @@ export const StyledAnchor = () => (
 export const CustomButton = () => (
   <ShareButton
     {...args}
-    anchor={
+    anchorButton={
       <Button
         variant="contained"
         startIcon={<ShareLocationIcon />}
